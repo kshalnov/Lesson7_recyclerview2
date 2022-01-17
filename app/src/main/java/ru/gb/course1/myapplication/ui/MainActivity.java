2c9bb3e4c53f7f22500969ba6de04f1c156e5f10
Kirill Shalnov <borhammere@gmail.com>
2022-01-17 22:25:09 +0300

    fragments experiment

--- app/src/main/java/ru/gb/course1/myapplication/ui/MainActivity.java
+++ app/src/main/java/ru/gb/course1/myapplication/ui/MainActivity.java
@@ -1,7 +1,10 @@
 package ru.gb.course1.myapplication.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
@@ -19,11 +22,11 @@ public class MainActivity
     private static final String TAG_LIST_FRAGMENT = "TAG_LIST_FRAGMENT";
     private static final String TAG = "@@@_MainActivity";
 
-//    private FrameLayout secondFragmentContainer;
+    private FrameLayout secondFragmentContainer;
 
-//    private boolean isTwoPaneMode() {
-//        return secondFragmentContainer != null;
-//    }
+    private boolean isTwoPaneMode() {
+        return secondFragmentContainer != null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,41 +34,49 @@ public class MainActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        secondFragmentContainer = findViewById(R.id.activity_main__second_fragment_container);
+        secondFragmentContainer = findViewById(R.id.activity_main__second_fragment_container);
 
-//        if (isTwoPaneMode()) {
-//            // 1 узнать какой фрагмент лежит в контейнере
-//            Fragment mainFragment = getSupportFragmentManager().findFragmentById(R.id.activity_main__main_fragment_container);
-//            if (mainFragment instanceof ColorsListFragment) {
-//                // всё хорошо
-//            } else if (mainFragment != null) {
-//                // переложить фрагмент в другой контейнер
-//                putFragmentToSecondContainer(mainFragment);
-//                showListInMainContainer();
-//            } else {
-//                showListInMainContainer();
-//            }
-//        } else {
-//            // тоже нужно что-то придумать
-//            // todo @@@
-//            showListInMainContainer();
-//        }
-        if (savedInstanceState == null) {
+        if (isTwoPaneMode()) {
+            // 1 узнать какой фрагмент лежит в контейнере
+            Fragment mainFragment = getSupportFragmentManager().findFragmentById(R.id.activity_main__main_fragment_container);
+            if (mainFragment instanceof ColorsListFragment) {
+                // всё хорошо
+            } else if (mainFragment != null) {
+                // переложить фрагмент в другой контейнер
+                moveFragment(mainFragment);
+                showListInMainContainer();
+            } else {
+                showListInMainContainer();
+            }
+        } else {
+            // тоже нужно что-то придумать
+            // todo @@@
             showListInMainContainer();
         }
     }
-//
-//    private void putFragmentToSecondContainer(Fragment fragment) {
-//        final Bundle args = fragment.getArguments();
-//        final Bundle savedInstanceState = new Bundle();
-//        fragment.onSaveInstanceState(savedInstanceState);
-//        getSupportFragmentManager()
-//                .beginTransaction()
-//                .remove(fragment)
-//                .replace(R.id.activity_main__second_fragment_container, fragment)
-//                .commit();
-//    }
-//
+
+    private void moveFragment(Fragment oldFragment) {
+        final Bundle args = oldFragment.getArguments();
+        final Bundle savedState = new Bundle();
+        oldFragment.onSaveInstanceState(savedState);
+
+        Fragment newFragment = null;
+        try {
+            newFragment = oldFragment.getClass().newInstance();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+        newFragment.setArguments(args);
+
+        newFragment.onAttach((Context) this);
+        FrameLayout container = findViewById(R.id.activity_main__second_fragment_container);
+        final View fragmentView = newFragment.onCreateView(LayoutInflater.from(this), container, savedState);
+        container.addView(fragmentView);
+        newFragment.onViewCreated(fragmentView, savedState);
+    }
+
     private void showListInMainContainer() {
         Fragment colorsListFragment = new ColorsListFragment();
         getSupportFragmentManager()
@@ -81,12 +92,12 @@ public class MainActivity
         Fragment colorDetailsFragment = ColorDetailsFragment.newInstance(colorEntity);
         Log.d(TAG, "Begin ColorDetailsFragment transaction");
 
-        int containerId = R.id.activity_main__second_fragment_container;
-//        if (isTwoPaneMode()) {
-//            containerId = R.id.activity_main__second_fragment_container;
-//        } else {
-//            containerId = R.id.activity_main__main_fragment_container;
-//        }
+        int containerId;
+        if (isTwoPaneMode()) {
+            containerId = R.id.activity_main__second_fragment_container;
+        } else {
+            containerId = R.id.activity_main__main_fragment_container;
+        }
 
         getSupportFragmentManager()
                 .beginTransaction()
